5d32beccae5e784d57844aa473df46c2855d6808
cserna98 <100805051+cserna98@users.noreply.github.com>
2022-12-05 17:24:26 -0500

    creando proyecto clinica odontologica

---
+++ src/test/java/com/Integrador/Integrador_proyectoOdntologico/IntegradorProyectoOdntologicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Integrador.Integrador_proyectoOdntologico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorProyectoOdntologicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
